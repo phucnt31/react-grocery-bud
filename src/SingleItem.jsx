b17e3d4b34e95af644ba91ea9ead9f74d9bf56f7
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-19 20:05:28 +0700

    Render SingleItem.jsx

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-const SingleItem = () => {
-  return <div>SingleItem</div>;
+const SingleItem = ({ completed, name }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        value={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <p
+        style={{
+          textTransform: "capitalize",
+          textDecoration: isChecked && "line-through",
+        }}
+      >
+        {name}
+      </p>
+    </div>
+  );
 };
 
 export default SingleItem;
