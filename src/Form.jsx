6b7c0ef07abc33f8bb705817d405fd858540bca1
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-19 19:38:33 +0700

    Add field to form component

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
-  return <div>Form</div>;
+  const [itemName, setItemName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>grocery bud</h4>
+      <div className="form-control">
+        <input
+          type="text"
+          className="form-input"
+          value={itemName}
+          onChange={(e) => setItemName(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          add item
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default Form;
