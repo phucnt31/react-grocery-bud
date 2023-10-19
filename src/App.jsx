8e959a8ab567279f6bae9d3e8ece5284790ac896
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-19 19:32:25 +0700

    Setup state and create Form.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+import Form from "./Form";
+
 const App = () => {
-  return <h2>Grocery Bud - Starter</h2>;
+  const [items, setItems] = useState([]);
+
+  return (
+    <main>
+      <Form />
+    </main>
+  );
 };
 
 export default App;
